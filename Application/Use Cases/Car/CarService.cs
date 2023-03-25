2e1ba0d1e6d3fb1ae5ba66d2ebd8048cf6880f39
Sebastian Velez <sebastian@Sebastians-iMac.local>
2023-03-24 23:33:45 -0500

    application

---
+++ Application/Use Cases/Car/CarService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Application.UseCases.Car
+{
+	public class CarService
+	{
+
+        public void CreateCar(Domain.Entities.Car car)
+        {
+        }
+
+        public void UpdateCar(Domain.Entities.Car car)
+        {
+        }
+
+        public void DeleteCar(Domain.Entities.Car car)
+        {
+        }
+    }
+}
+
