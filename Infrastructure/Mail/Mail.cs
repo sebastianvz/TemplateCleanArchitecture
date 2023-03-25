100df1657d1ea9d39f2cc5386be927a52e09c4d2
Sebastian Velez <sebastian@Sebastians-iMac.local>
2023-03-24 23:37:41 -0500

    infrastructure

---
+++ Infrastructure/Mail/Mail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Infrastructure.Mail
+{
+	public class Mail
+	{
+		public Mail()
+		{
+		}
+
+        public void sendMail(Mail mail)
+        {
+            Console.WriteLine("sending mail ...");
+        }
+    }
+}
+
