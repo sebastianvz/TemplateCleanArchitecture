33959e9654ed934fd44c6d77f652c222cb2a1752
Sebastian Velez <sebastian@Sebastians-iMac.local>
2023-03-24 23:29:46 -0500

    Domain

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Domain.Entities
+{
+	public class Car
+	{
+        public string name { get; set; }
+        public int year { get; set; }
+        public string model { get; set; }
+        public float price { get; set; }
+        public string brand { get; set; }
+
+        public void StarEngine()
+        {
+
+            Console.WriteLine("starting car engine ...");
+        }
+
+        public void StopEngine()
+        {
+            Console.WriteLine("stopiong car engine ...");
+        }
+
+        public void Break()
+        {
+            Console.WriteLine("breaking car...");
+        }
+    }
+}
+
