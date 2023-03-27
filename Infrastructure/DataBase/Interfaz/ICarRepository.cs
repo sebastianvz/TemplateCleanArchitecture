883c07a92977e7925f4f32a76c3de43b98c180a4
Sebastian Velez <sebastian@Sebastians-iMac.local>
2023-03-27 13:12:36 -0500

    Repository pattern

---
+++ Infrastructure/DataBase/Interfaz/ICarRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Infrastructure.DataBase.Entities;
+
+namespace Infrastructure.DataBase.Interfaz
+{
+	public interface ICarRepository 
+	{
+        public void CreateCar(Car car);
+
+        public void UpdateCar(Car car);
+
+        public void DeleteCar(int carID);
+
+        public IEnumerable<Car> GetAll();
+
+        public Car GetById(int carID);
+
+        public void Dispose(int carID);
+    }
+}
+
---
+++ Infrastructure/DataBase/Repositorys/CarRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Infrastructure.DataBase.Entities;
+using Infrastructure.DataBase.Interfaz;
+
+namespace Infrastructure.DataBase.Repositorys
+{
+	public class CarRepository : ICarRepository
+    {
+        public void CreateCar(Car car)
+        {
+        }
+
+        public void UpdateCar(Car car)
+        {
+        }
+
+        public void DeleteCar(int carID)
+        {
+        }
+
+        public IEnumerable<Car> GetAll()
+        {
+            return null;
+        }
+
+        public Car GetById(int carID)
+        {
+            return null;
+        }
+
+        public void Dispose(int carID)
+        {
+        }
+
+
+
+    }
+}
+
