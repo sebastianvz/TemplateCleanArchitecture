99ac17315a48717fab75a406c515c2e2a0f95cf0
Sebastian Velez <sebastian@Sebastians-iMac.local>
2023-03-29 20:42:55 -0500

    update usecases with methods from repository pattern

---
+++ Application/Use Cases/CarFunctions/CarService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using Application.Contracts;
+using Domain.Entities;
+
+namespace Application.UseCases.CarFunctions
+{
+	public class CarService
+	{
+
+        private readonly ICarRepository _carRepository;
+
+        public CarService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public void CreateCar(Car car)
+        {
+            _carRepository.CreateCar(car);
+        }
+
+        public void UpdateCar(Car car)
+        {
+            _carRepository.UpdateCar(car);
+        }
+
+        public void DeleteCar(Car car)
+        {
+            _carRepository.DeleteCar(car.IdCar);
+        }
+    }
+}
+
