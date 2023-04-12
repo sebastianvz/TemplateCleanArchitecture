becda8b76efbd314896f7d1baba98288b2b48073
Sebastian Velez <sebastian@Sebastians-iMac.local>
2023-04-12 16:06:52 -0500

    Implementando TDD - Verde

--- TemplateCleanArchitectureTest/Appplication/UseCases/CarServicesTest.cs
+++ TemplateCleanArchitectureTest/Appplication/UseCases/CarServicesTest.cs
@@ -1,5 +1,8 @@
 ﻿using Xunit;
 using Domain.Entities;
+using Application.UseCases.CarFunctions;
+using Moq;
+using Application.Contracts;
 
 namespace TemplateCleanArchitectureTest.Appplication.UseCases
 {
@@ -8,7 +11,24 @@ namespace TemplateCleanArchitectureTest.Appplication.UseCases
 		[Fact()]
 		public void SusccessCarCreation()
         {
-			throw new Exception("Rojo TDD");
+			//Setup
+			Car car = new Car()
+			{
+				Brand = "Audi",
+				Year = 2022,
+				Price = 6000,
+				Name = "RS"
+			};
+
+
+			var carRepository = new Mock<ICarRepository>();
+			var carService = new CarService(carRepository.Object);
+
+			//action
+			carService.CreateCar(car);
+
+			carRepository.Verify(x => x.Insert(car), Times.Once);
+
 		}
 
 	}
